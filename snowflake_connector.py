9169a1b4988a5c809045b1b29da088cd418c0fe4
SonAcx <sonia.cuevasromero@acxion.com>
2025-10-23 13:58:14 -0700

    Update snowflake_connector.py

--- snowflake_connector.py
+++ snowflake_connector.py
@@ -4,7 +4,6 @@ import pandas as pd
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from concurrent.futures import ThreadPoolExecutor
-import time
 
 def get_snowflake_connection():
     # Load the private key properly
